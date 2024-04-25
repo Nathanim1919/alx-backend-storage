3a472e9ea31003b8e84884bffe339e1fee41e2fb
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-25 07:20:08 +0000

    add comment above the script

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,9 @@
+-- Create atable users with below attributes
+-- id, integer, never null, auto increment and primary key
+-- email, string (255 characters), never null and unique
+-- name, string (255 characters)
+-- if table exists, script will not fail, can be executed on any database
+
 CREATE TABLE IF NOT EXISTS users(
 	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
 	email VARCHAR(255) NOT NULL UNIQUE,

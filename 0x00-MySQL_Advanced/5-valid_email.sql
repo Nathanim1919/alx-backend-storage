58d9f8b2d5d38c2886391b02e712b8e58d72c50c
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-25 11:34:05 +0000

    Email validation to sent

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -6,8 +6,8 @@ DELIMITER $$ ;
 CREATE TRIGGER reset_valid_email
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-	IF NEW.email <> OLD.email THEN
-		UPDATE users SET valid_email = 0 WHERE id = NEW.id;
+	IF NEW.email != OLD.email THEN
+		SET NEW.valid_email = 0;
 	END IF;
 END;
 DELIMITER ;

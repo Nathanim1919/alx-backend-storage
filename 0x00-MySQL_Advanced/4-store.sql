dece6cfe74cf1966f5050f062f4cb76bc079bfe5
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-25 09:18:19 +0000

    Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- a SQL script that creates a trigger that
-	-- decreases the quantity of an item 
-		--- after adding a new order.
+-- decreases the quantity of an item 
+-- after adding a new order.
 -- Quantity in the table items can be negative.
 
 CREATE TRIGGER decrease_item_quantity

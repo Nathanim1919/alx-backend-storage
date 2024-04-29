2cf106748b24cd517fea7bd873a55b607c66c1eb
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-29 18:08:08 +0300

    Log stats - new version

---
+++ 0x01-NoSQL/102-log_stats.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""Log stats"""
+
+from pymongo import MongoClient
+
+
+def log_stats(mongo_collection):
+    """Log stats"""
+    print(f"{mongo_collection.estimated_document_count()} logs")
+    print("Methods:")
+    print(f"\tmethod GET: {mongo_collection.count_documents({'method': 'GET'})}")
+    print(f"\tmethod POST: {mongo_collection.count_documents({'method': 'POST'})}")
+    print(f"\tmethod PUT: {mongo_collection.count_documents({'method': 'PUT'})}")
+    print(f"\tmethod PATCH: {mongo_collection.count_documents({'method': 'PATCH'})}")
+    print(f"\tmethod DELETE: {mongo_collection.count_documents({'method': 'DELETE'})}")
+    print(f"{mongo_collection.count_documents({'method': 'GET', 'path': '/status'})} status check")
+    print("IPs:")
+    ips = mongo_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}}
+    ])
+    for ip in ips:
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    logs_collection = client.logs.nginx
+    log_stats(logs_collection)

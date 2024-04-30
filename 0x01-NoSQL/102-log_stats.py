bedef7a32a8fab488a6b1c9d050bf298822b0b32
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-30 09:30:49 +0300

    log stats

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,31 +1,40 @@
 #!/usr/bin/env python3
-"""Log stats"""
-
+"""
+Log stats
+"""
 from pymongo import MongoClient
 
 
 def log_stats():
-    """Log stats"""
+    """ log_stats.
+    """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    mongo_collection = client.logs.nginx
-    print(f"{mongo_collection.estimated_document_count()} logs")
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
+    print(f"{total} logs")
     print("Methods:")
-    print(f"\tmethod GET: {mongo_collection.count_documents({'method': 'GET'})}")
-    print(f"\tmethod POST: {mongo_collection.count_documents({'method': 'POST'})}")
-    print(f"\tmethod PUT: {mongo_collection.count_documents({'method': 'PUT'})}")
-    print(f"\tmethod PATCH: {mongo_collection.count_documents({'method': 'PATCH'})}")
-    print(f"\tmethod DELETE: {mongo_collection.count_documents({'method': 'DELETE'})}")
-    print(f"{mongo_collection.count_documents({'method': 'GET', 'path': '/status'})} status check")
+    print(f"\tmethod GET: {get}")
+    print(f"\tmethod POST: {post}")
+    print(f"\tmethod PUT: {put}")
+    print(f"\tmethod PATCH: {patch}")
+    print(f"\tmethod DELETE: {delete}")
+    print(f"{path} status check")
     print("IPs:")
-    ips = mongo_collection.aggregate([
-        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
-        {"$sort": {"count": -1}}
-    ])
+    sorted_ips = logs_collection.aggregate(
+        [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+         {"$sort": {"count": -1}}])
     i = 0
-    for ip in ips:
+    for s in sorted_ips:
         if i == 10:
             break
-        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+        print(f"\t{s.get('_id')}: {s.get('count')}")
         i += 1
 
 

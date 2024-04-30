60d9eda90aac95425844ef37dbd15ac098eaaabd
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-30 09:18:33 +0300

    list all documents

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -3,7 +3,12 @@
 
 
 def list_all(mongo_collection):
+    """List all documents in a collection
+    Args:
+        mongo_collection: Collection object
+    Returns: List of documents or empty list
+    """
     if (mongo_collection.count() == 0):
         return []
     else:
-        return list(mongo_collection.find())
+        return mongo_collection.find()

7cc9c01c51c22c4c3f7f8f4ee30b80d65bf40260
Nathanim1919 <nathanim2tadele@gmail.com>
2024-04-30 09:23:33 +0300

    update topics

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -4,6 +4,6 @@
 
 def update_topics(mongo_collection, name, topics):
     """ Update topics """
-    return mongo_collection.update(
+    return mongo_collection.update_many(
         {"name": name}, {"$set": {"topics": topics}}
     )
